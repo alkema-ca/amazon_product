d425e493f6f0a1e481f2794859500f6cfff3d13a
Jody Alkema <jody@alkema.ca>
2018-09-10 15:13:13 -0700

    Add validations to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,8 @@ class Product < ApplicationRecord
   has_many :categories, dependent: :destroy
   has_one :page_request, dependent: :destroy
 
+  validates :asin, presence: true, uniqueness: true
+
   def grouped_category_sets
     categories.order('id asc, set_id asc').group_by(&:set_id)
   end
